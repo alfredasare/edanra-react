29a8cce8a5389121427ad7250a595359b9506e97
Alfred Amoah <alfredasare101@gmail.com>
2020-03-31 12:01:12 +0000

    scroll to top fixed

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
 import store from "./redux/store";
+import ScrollToTop from "./utils/scroll-to-top";
 
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
+            <ScrollToTop/>
             <App/>
         </BrowserRouter>
     </Provider>,
