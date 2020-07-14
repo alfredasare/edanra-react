4a1a29815e3d2d513bcdd877ed1670b16e9e1c63
Alfred Asare <alfredasare101@gmail.com>
2020-07-14 12:42:26 +0000

    made ad cards links to properties

--- src/components/property-item/property-item.component.jsx
+++ src/components/property-item/property-item.component.jsx
@@ -6,20 +6,22 @@ const PropertyItem = ({routeName, main_image_url, town, region, username, price,
 
     return (
         <div style={{marginTop: '20px'}} className="col-md-4">
-            <div className="slide-item">
-                <div className="item-image">
-                    <img src={main_image_url}
-                         alt={`${property_type} at ${region}`}/>
-                </div>
-                <div className="item-content">
-                    <div className="item-location">{town} &#9679; {region}</div>
-                    <div className="item-name">{property_type} hosted by {username}</div>
-                    <div className="item-price">Ghc {price} / month &#9679; <span
-                        className="negotiation-status">{negotiation_status}</span>
+            <Link to={`properties/${routeName}`}>
+                <div className="slide-item">
+                    <div className="item-image">
+                        <img src={main_image_url}
+                             alt={`${property_type} at ${region}`}/>
                     </div>
-                    <Link className="workspace-link" to={`properties/${routeName}`}>View Space</Link>
+                    <div className="item-content">
+                        <div className="item-location">{town} &#9679; {region}</div>
+                        <div className="item-name">{property_type} hosted by {username}</div>
+                        <div className="item-price">Ghc {price} / month &#9679; <span
+                            className="negotiation-status">{negotiation_status}</span>
+                        </div>
+                        <Link className="workspace-link" to={`properties/${routeName}`}>View Space</Link>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 };
